cf83201d72ce3af41f4920c92100d761c34e2582
MrYaroslav <xiaomiredist@gmail.com>
2023-02-13 20:54:52 +0200

    design edition

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('registration', show_reg_form, name='reg_form'),
-    path('login', show_log_form, name='log_form'),
-    path('shop', show_shop, name='shop'),
+    path('registration/', show_reg_form, name='reg_form'),
+    path('', show_log_form, name='log_form'),
+    path('shop/', show_shop, name='shop'),
 ]
